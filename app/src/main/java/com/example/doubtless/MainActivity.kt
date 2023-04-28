cf0d74d6ef8ddff7b05aa0955399afb2471b1db5
sidsharma2002 <rooter@rooters-MacBook-Pro.local>
2023-04-28 09:51:02 +0530

    basic setup initial

--- app/src/main/java/com/example/doubtless/MainActivity.kt
+++ app/src/main/java/com/example/doubtless/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.doubtless
 
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,23 +20,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupNavigation()
-    }
-
-    private fun setupNavigation() {
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard
-            )
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        window.navigationBarColor = Color.BLACK
     }
 }
