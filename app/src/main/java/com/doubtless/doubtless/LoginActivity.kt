b95fac136fe6ba6467c71fda5a7a717a135a3aa7
krishnaTech6 <krishna060702@gmail.com>
2023-05-03 10:16:30 +0530

    google login setup completed

---
+++ app/src/main/java/com/doubtless/doubtless/LoginActivity.kt
@@ -0,0 +1,68 @@
+package com.doubtless.doubtless
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.doubtless.doubtless.theming.retro.RetroLayout
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        findViewById<RetroLayout>(R.id.btn_signin).setOnClickListener{
+
+            progressBar = findViewById(R.id.progress)
+            progressBar.visibility = View.VISIBLE
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, 1001)
+
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode== 1001){
+
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            val i = Intent(this, MainActivity::class.java)
+                            startActivity(i)
+
+                            progressBar = findViewById(R.id.progress)
+                            progressBar.visibility = View.GONE
+
+                        }else{
+                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+        }
+}
