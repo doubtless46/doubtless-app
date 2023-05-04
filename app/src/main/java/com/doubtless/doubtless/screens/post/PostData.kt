578fc3f7f3dbf4d07a8522fb723c6eb9a12ef8f7
Aditya <adityasharma131003@gmail.com>
2023-05-04 18:06:55 +0530

    Add Upvote and Downvote Feature and Refactor Code
    - Replaced all references to "Post" with "Doubts" in file names and variable names for clarity
    - Refactored code to include Upvote and Downvote feature for Doubts
    - Changed Upvote and Downvote hashmaps to use List of Strings for better performance and memory usage
    - Added null safety checks to CreateDoubtFragment to reduce app crashes caused by null values
    - Added warning message when back button is pressed in CreateDoubtFragment and heading or description is not null to prevent accidental loss of data.

--- app/src/main/java/com/doubtless/doubtless/screens/post/PostData.kt
+++
@@ -1,9 +0,0 @@
-package com.doubtless.doubtless.screens.post
-
-data class PostData(
-    var userName: String,
-    var time: String,
-    var doubt: String,
-    var heading: String,
-    var description: String
-)
