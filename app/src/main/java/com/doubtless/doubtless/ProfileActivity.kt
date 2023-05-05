e4df606019cd2f617fb1564c8697174916c020ae
krishnaTech6 <krishna060702@gmail.com>
2023-05-05 13:55:38 +0530

    update: view binding instead of android extension and other changes

--- app/src/main/java/com/doubtless/doubtless/ProfileActivity.kt
+++ app/src/main/java/com/doubtless/doubtless/ProfileActivity.kt
@@ -4,33 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.doubtless.doubtless.databinding.ActivityLoginBinding
+import com.doubtless.doubtless.databinding.ActivityProfile2Binding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_profile2.*
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
+    private var _binding : ActivityProfile2Binding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile2)
 
+        _binding = ActivityProfile2Binding.inflate(layoutInflater)
+
         mAuth = FirebaseAuth.getInstance()
 
-        tv_name.text = mAuth.currentUser?.displayName
-        tv_user_email.text = mAuth.currentUser?.email
+        binding.tvName.text = mAuth.currentUser?.displayName
+        binding.tvUserEmail.text = mAuth.currentUser?.email
 
 
-        btn_signout.setOnClickListener {
+        binding.btnSignout.setOnClickListener {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
 
-            val googlesigninclient = GoogleSignIn.getClient(this,gso)
-            googlesigninclient.signOut()
+            val googleSignInClient = GoogleSignIn.getClient(this,gso)
+            googleSignInClient.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
