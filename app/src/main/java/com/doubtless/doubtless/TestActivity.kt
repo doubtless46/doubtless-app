f67809e4da67060f0572ca2aac62957e6a2d4316
Aditya <77337791+Aditya13s@users.noreply.github.com>
2023-05-06 17:36:34 +0530

    Post screen (#7)
    * Create Post Screen Added
    
    * Refactor code and add View Post screen
    
    - Removed unused code to improve codebase cleanliness
    - Renamed "posts" collection to "AllPosts" for better clarity
    - Deleted unused CreatePostViewModel.kt file
    - Created post_layout.xml to define the layout for each post item
    - Added ViewPostAdapter.kt to display posts in ViewPostFragment
    - Created PostData.kt to define the data model for each post
    - Added fetchAllPosts() function to ViewPostViewModel for getting all posts
    - Added getDateTime() function to convert Timestamp to Date object
    - Updated ViewPostFragment to use ViewPostAdapter and display all posts
    - Changed "question" field to "doubt" and added "heading" and "description" fields
    - Updated fragment_create_post.xml and fragment_view_post.xml to reflect new fields
    
    This commit addresses feedback received during code review and adds the functions to display all posts in ViewPostFragment.kt
    
    * Add Upvote and Downvote Feature and Refactor Code
    
    - Replaced all references to "Post" with "Doubts" in file names and variable names for clarity
    - Refactored code to include Upvote and Downvote feature for Doubts
    - Changed Upvote and Downvote hashmaps to use List of Strings for better performance and memory usage
    - Added null safety checks to CreateDoubtFragment to reduce app crashes caused by null values
    - Added warning message when back button is pressed in CreateDoubtFragment and heading or description is not null to prevent accidental loss of data.
    
    * bottom nav working
    
    * bottom nav working
    
    * bottom nav working
    
    * Resolve Merge Conflicts
    
    * Resolve Merge Conflicts
    
    ---------
    
    Co-authored-by: sidsharma2002 <rooter@rooters-MacBook-Pro.local>

---
+++ app/src/main/java/com/doubtless/doubtless/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.doubtless.doubtless
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
