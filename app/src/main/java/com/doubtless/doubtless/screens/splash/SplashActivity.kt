ae11344669bc4072e58ebd2a96bf5c94f928f956
Aditya <adityasharma131003@gmail.com>
2023-05-24 19:28:55 +0530

    Feedback and Force App Update features
    - Added submit feedback button in DashboardFragment to redirect to the Gmail app with Developer's Email
    - Implemented Force App Update feature, checking minimum version requirements from remote config and displaying update dialog

--- app/src/main/java/com/doubtless/doubtless/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/splash/SplashActivity.kt
@@ -3,12 +3,19 @@ package com.doubtless.doubtless.screens.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.doubtless.doubtless.BuildConfig
 import com.doubtless.doubtless.DoubtlessApp
 import com.doubtless.doubtless.R
+import com.doubtless.doubtless.screens.auth.usecases.UserManager
 import com.doubtless.doubtless.utils.anims.animateFadeUp
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var userManager: UserManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -18,27 +25,59 @@ class SplashActivity : AppCompatActivity() {
             delay(200L)
             view.animateFadeUp(800L)
 
-            val userManager = DoubtlessApp.getInstance().getAppCompRoot().getUserManager()
+            userManager = DoubtlessApp.getInstance().getAppCompRoot().getUserManager()
+            checkMinAppVersionRequired()
+        }
+    }
 
-            if (userManager.getLoggedInUser() != null) {
-                DoubtlessApp.getInstance()
-                    .getAppCompRoot().router.moveToMainActivity(this@SplashActivity)
+    private fun moveToNextActivity() {
+        if (userManager.getLoggedInUser() != null) {
+            DoubtlessApp.getInstance()
+                .getAppCompRoot().router.moveToMainActivity(this@SplashActivity)
 
-                val analyticsTracker =
-                    DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
-                analyticsTracker.trackAppLaunch()
+            val analyticsTracker = DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
+            analyticsTracker.trackAppLaunch()
 
-                finish()
-            } else {
-                DoubtlessApp.getInstance()
-                    .getAppCompRoot().router.moveToLoginActivity(this@SplashActivity)
+            finish()
+        } else {
+            DoubtlessApp.getInstance()
+                .getAppCompRoot().router.moveToLoginActivity(this@SplashActivity)
 
-                val analyticsTracker =
-                    DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
-                analyticsTracker.trackAppLaunch()
+            val analyticsTracker = DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
+            analyticsTracker.trackAppLaunch()
 
-                finish()
+            finish()
+        }
+    }
+
+    private fun checkMinAppVersionRequired() {
+        val currVersion = BuildConfig.VERSION_CODE
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        var minVersion = 0
+        val configSettings =
+            FirebaseRemoteConfigSettings.Builder().setMinimumFetchIntervalInSeconds(10).build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                minVersion = remoteConfig.getLong("min_app_version").toInt();
+                if (minVersion > currVersion) {
+                    showAppUpdateDialog()
+                } else {
+                    moveToNextActivity()
+                }
             }
         }
+    }
+
+    private fun showAppUpdateDialog() {
+        val dialog = AlertDialog.Builder(this).apply {
+            title = "New Version Available"
+            setMessage("Please, update app to new version to continue")
+            setPositiveButton("Update") { _, _ ->
+                moveToNextActivity()
+            }
+        }
+        dialog.setCancelable(false)
+        dialog.show()
     }
 }
