a240100c9695f7fc602c822d2dcac6781606bbf1
Vishesh Pahwa <73529277+visheshpahwa@users.noreply.github.com>
2023-05-26 20:52:00 +0530

    added on boarding activity (#17)

--- app/src/main/java/com/doubtless/doubtless/navigation/Router.kt
+++ app/src/main/java/com/doubtless/doubtless/navigation/Router.kt
@@ -3,6 +3,7 @@ package com.doubtless.doubtless.navigation
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.doubtless.doubtless.OnBoardingActivity
 import com.doubtless.doubtless.screens.auth.LoginActivity
 import com.doubtless.doubtless.screens.main.MainActivity
 
@@ -19,7 +20,7 @@ class Router {
     }
 
     fun moveToOnBoardingActivity(activity: Activity) {
-        val i = Intent(activity, MainActivity::class.java) // fixme
+        val i = Intent(activity, OnBoardingActivity::class.java) // fixme
         activity.startActivity(i)
     }
 
