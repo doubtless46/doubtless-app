641d482b89647eb7b34916f27b346ae54cc4512a
Aditya <adityasharma131003@gmail.com>
2023-05-27 20:44:33 +0530

    Post Doubt Changes
    - Post Doubt Character Limit
    - Confirm when user clicks on post doubt screen
    - Shared Preference to store post
    - Show Profile Picture using Glide

--- app/src/main/java/com/doubtless/doubtless/screens/doubt/CreateDoubtFragment.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/doubt/CreateDoubtFragment.kt
@@ -1,15 +1,17 @@
 package com.doubtless.doubtless.screens.doubt
 
-import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.InputFilter
 import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.activity.addCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.doubtless.doubtless.DoubtlessApp
@@ -20,8 +22,12 @@ import com.doubtless.doubtless.screens.auth.usecases.UserManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.gson.Gson
 import java.util.Date
 import java.util.UUID
+import kotlin.properties.Delegates
 
 class CreateDoubtFragment : Fragment() {
     private var _binding: FragmentCreateDoubtBinding? = null
@@ -32,6 +38,13 @@ class CreateDoubtFragment : Fragment() {
     private lateinit var db: FirebaseFirestore
     private lateinit var userManager: UserManager
     private lateinit var analyticsTracker: AnalyticsTracker
+
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+    private lateinit var configSettings: FirebaseRemoteConfigSettings
+    private var maxHeadingCharLimit by Delegates.notNull<Int>()
+    private var maxDescriptionCharLimit by Delegates.notNull<Int>()
+    private lateinit var sharedPreferences: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,42 +63,60 @@ class CreateDoubtFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+        configSettings =
+            FirebaseRemoteConfigSettings.Builder().setMinimumFetchIntervalInSeconds(10).build()
+
         binding.doubtHeading.setRawInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES or InputType.TYPE_TEXT_FLAG_MULTI_LINE)
         binding.doubtHeading.requestFocus()
         val mgr = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         mgr.showSoftInput(binding.doubtHeading, InputMethodManager.SHOW_FORCED)
+
+        getMaxCharacterLimit()
+
+        sharedPreferences = requireContext().getSharedPreferences("post_data", Context.MODE_PRIVATE)
 
         binding.postButton.setOnClickListener {
             if (!isButtonClicked) {
                 checkText()
             }
         }
+        val (savedHeadingText, savedDescriptionText) = getSharedPreferencesData()
 
-        binding.close.setOnClickListener {
-            handleBackPress()
+        savedHeadingText?.let {
+            if (it.isNotEmpty()) {
+                binding.doubtHeading.setText(savedHeadingText)
+            }
         }
+
+        savedDescriptionText?.let {
+            if (it.isNotEmpty()) {
+                binding.doubtDescription.setText(savedDescriptionText)
+            }
+        }
+
 
     }
 
-    private fun handleBackPress() {
-        if (binding.doubtHeading.text.toString()
-                .isNotEmpty() || binding.doubtDescription.text.toString().isNotEmpty()
-        ) {
-            showWarningDialog()
-        } else {
-            requireActivity().finish()
-        }
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(context, "OnViewDestroyed", Toast.LENGTH_SHORT).show()
+        saveTextToSharedPreferences(
+            binding.doubtHeading.text.toString(), binding.doubtDescription.text.toString()
+        )
     }
 
-    private fun showWarningDialog() {
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle("Are you sure?")
-        builder.setMessage("Do you want to go back?")
-        builder.setPositiveButton("Yes") { _, _ ->
-            requireActivity().finish()
-        }
-        builder.setNegativeButton("No") { _, _ -> }
-        builder.show()
+    private fun getSharedPreferencesData(): Pair<String?, String?> {
+        val headingText = sharedPreferences.getString("headingText", null)
+        val descriptionText = sharedPreferences.getString("descriptionText", null)
+        return Pair(headingText, descriptionText)
+    }
+
+    private fun saveTextToSharedPreferences(headingText: String, descriptionText: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("headingText", headingText)
+        editor.putString("descriptionText", descriptionText)
+        editor.apply()
     }
 
 
@@ -101,11 +132,55 @@ class CreateDoubtFragment : Fragment() {
             binding.postButton.isClickable = false
             binding.postButton.alpha = 0.8f
 
-            createDoubt(
-                binding.doubtHeading.text.toString(), binding.doubtDescription.text.toString(),
-                userManager.getCachedUserData()!!
-            )
+            showConfirmationDialog()
+
         }
+    }
+
+    private fun showConfirmationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Confirmation").setMessage("Are you sure you want to post?")
+            .setPositiveButton("Post") { dialogInterface: DialogInterface, _: Int ->
+                createDoubt(
+                    binding.doubtHeading.text.toString(),
+                    binding.doubtDescription.text.toString(),
+                    userManager.getCachedUserData()!!
+                )
+                dialogInterface.dismiss()
+            }.setNegativeButton("Cancel") { dialogInterface: DialogInterface, _: Int ->
+                isButtonClicked = false
+                binding.postButton.alpha = 1f
+                dialogInterface.dismiss()
+            }.show()
+    }
+
+    private fun getMaxCharacterLimit() {
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val maxCharLimit = remoteConfig.getString("max_character_limit")
+                try {
+                    val jsonObject = Gson().fromJson(maxCharLimit, Map::class.java) as Map<*, *>
+                    maxHeadingCharLimit = (jsonObject["max_heading_char_limit"] as Double).toInt()
+                    maxDescriptionCharLimit =
+                        (jsonObject["max_description_char_limit"] as Double).toInt()
+                    setMaxCharacterLimit(maxHeadingCharLimit, maxDescriptionCharLimit)
+
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(context, "Try Again Later", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun setMaxCharacterLimit(maxHeadingCharLimit: Int, maxDescriptionCharLimit: Int) {
+        binding.doubtHeading.filters =
+            arrayOf<InputFilter>(InputFilter.LengthFilter(maxHeadingCharLimit))
+        binding.doubtDescription.filters =
+            arrayOf<InputFilter>(InputFilter.LengthFilter(maxDescriptionCharLimit))
     }
 
     private fun createDoubt(heading: String, description: String, user: User) {
@@ -126,6 +201,9 @@ class CreateDoubtFragment : Fragment() {
         db.collection("AllDoubts").add(doubt).addOnSuccessListener {
             isButtonClicked = false
             binding.postButton.alpha = 1f
+            binding.doubtHeading.setText("")
+            binding.doubtDescription.setText("")
+
             Toast.makeText(context, "Posted Successfully!", Toast.LENGTH_SHORT).show()
         }.addOnFailureListener {
             isButtonClicked = false
@@ -138,5 +216,4 @@ class CreateDoubtFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
