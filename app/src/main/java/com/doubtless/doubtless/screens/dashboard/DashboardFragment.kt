e0cfc3a5edbe53d1a71cc0e60f96607a0d51b5e3
Aditya <adityasharma131003@gmail.com>
2023-06-02 21:30:42 +0530

    Implemented User Profile ViewHolder

--- app/src/main/java/com/doubtless/doubtless/screens/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/dashboard/DashboardFragment.kt
@@ -1,4 +1,5 @@
 package com.doubtless.doubtless.screens.dashboard
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -16,30 +17,37 @@ import com.doubtless.doubtless.databinding.FragmentDashboardBinding
 import com.doubtless.doubtless.screens.auth.usecases.UserManager
 import com.doubtless.doubtless.screens.common.GenericFeedAdapter
 import com.doubtless.doubtless.screens.doubt.DoubtData
-import com.doubtless.doubtless.screens.home.entities.FeedEntity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
 class DashboardFragment : Fragment() {
+
     private lateinit var mAuth: FirebaseAuth
     private lateinit var userManager: UserManager
     private var _binding: FragmentDashboardBinding? = null
     private lateinit var viewModel: DashboardViewModel
     private lateinit var adapter: GenericFeedAdapter
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
     private lateinit var tracker: AnalyticsTracker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tracker = DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
         mAuth = FirebaseAuth.getInstance()
         userManager = DoubtlessApp.getInstance().getAppCompRoot().getUserManager()
+
         viewModel = getViewModel()
         viewModel.fetchDoubts(forPageOne = true)
+
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -48,60 +56,90 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var lastRefreshed = System.currentTimeMillis()
 
-        val feedList = mutableListOf<FeedEntity>()
-        feedList.add(FeedEntity(FeedEntity.TYPE_USER_PROFILE, null))
+        binding.layoutSwipe.setOnRefreshListener {
+
+            if (System.currentTimeMillis() - lastRefreshed < 3 * 1000L) {
+                binding.layoutSwipe.isRefreshing = false
+                return@setOnRefreshListener
+            }
+
+            lastRefreshed = System.currentTimeMillis()
+
+            binding.layoutSwipe.isRefreshing = true
+            viewModel.refreshList()
+            adapter.clearCurrentList()
+        }
 
         if (!::adapter.isInitialized) {
-            adapter = GenericFeedAdapter(genericFeedEntities = feedList,
+            adapter = GenericFeedAdapter(genericFeedEntities = mutableListOf(),
                 onLastItemReached = {
                     viewModel.fetchDoubts()
                 },
                 interactionListener = object : GenericFeedAdapter.InteractionListener {
                     override fun onSearchBarClicked() {
                     }
+
+
                     override fun onDoubtClicked(doubtData: DoubtData, position: Int) {
+
                     }
+
                     override fun onSignOutClicked() {
                         tracker.trackLogout()
+
                         CoroutineScope(Dispatchers.Main).launch {
+
                             val result = withContext(Dispatchers.IO) {
                                 DoubtlessApp.getInstance().getAppCompRoot().getUserManager()
                                     .onUserLogoutSync()
                             }
+
                             if (!isAdded) return@launch
+
                             if (result is UserManager.Result.LoggedOut) {
+
                                 DoubtlessApp.getInstance()
                                     .getAppCompRoot().router.moveToLoginActivity(
                                         requireActivity()
                                     )
                                 requireActivity().finish()
+
                             } else if (result is UserManager.Result.Error) {
+
                                 Toast.makeText(
                                     this@DashboardFragment.requireContext(),
                                     result.message,
                                     Toast.LENGTH_LONG
                                 ).show() // encapsulate error ui handling
+
                             }
                         }
                     }
+
                     override fun onSubmitFeedbackClicked() {
                         tracker.trackFeedbackButtonClicked()
                         submitFeedback()
                     }
                 })
         }
+
+
+
         binding.doubtsRecyclerView.adapter = adapter
         binding.doubtsRecyclerView.layoutManager = LinearLayoutManager(context)
+
         viewModel.fetchedHomeEntities.observe(viewLifecycleOwner) {
             if (it == null) return@observe
             adapter.appendDoubts(it)
-            Log.i("ObserveFeed", it.toTypedArray().contentToString())
+            Log.i("ObserveFeed", it.size.toString())
             viewModel.notifyFetchedDoubtsConsumed()
+            binding.layoutSwipe.isRefreshing = false
         }
     }
 
@@ -115,6 +153,7 @@ class DashboardFragment : Fragment() {
             )
         )[DashboardViewModel::class.java]
     }
+
     private fun submitFeedback() {
         val intent = Intent(Intent.ACTION_SEND).apply {
             putExtra(Intent.EXTRA_EMAIL, arrayOf("doubtless46@gmail.com"))
@@ -125,7 +164,9 @@ class DashboardFragment : Fragment() {
             }
         }
         startActivity(intent)
+
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
