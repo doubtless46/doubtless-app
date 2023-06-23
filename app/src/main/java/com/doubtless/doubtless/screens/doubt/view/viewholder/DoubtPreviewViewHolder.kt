a8b1fee4831acf108464fa5033887bc943398ce5
PratthamArora <arora.prattham@gmail.com>
2023-06-23 20:17:16 +0530

    added animation to upvote and downvote icons for doubts

--- app/src/main/java/com/doubtless/doubtless/screens/doubt/view/viewholder/DoubtPreviewViewHolder.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/doubt/view/viewholder/DoubtPreviewViewHolder.kt
@@ -1,13 +1,10 @@
 package com.doubtless.doubtless.screens.doubt.view.viewholder
 
-import android.content.res.ColorStateList
 import android.text.util.Linkify
-import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -40,8 +37,8 @@ class DoubtPreviewViewHolder(
     private val description: TextView
     private val ivDp: ImageView
     private val tvNetVotes: TextView
-    private val ivUpvotes: ImageView
-    private val ivDownvotes: ImageView
+    private val ivUpvotes: CheckBox
+    private val ivDownvotes: CheckBox
     private val tvAnswers: TextView
     private val tvTags: TextView
     private val tvCollege: TextView
@@ -114,6 +111,7 @@ class DoubtPreviewViewHolder(
 
         setVotesUi(doubtData, votingUseCase)
 
+
         ivUpvotes.setOnClickListener {
             CoroutineScope(Dispatchers.Main).launch {
 
@@ -148,23 +146,16 @@ class DoubtPreviewViewHolder(
     }
 
     private fun setVotesUi(doubtData: DoubtData, votingUseCase: VotingUseCase) {
-        ivDownvotes.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_baseline_thumb_up_off_alt_24))
-
-        ivUpvotes.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_baseline_thumb_up_off_alt_24))
-        // ivUpvotes.setColorFilter(ContextCompat.getColor(itemView.context, R.color.grey), android.graphics.PorterDuff.Mode.SRC_IN);
-
         tvNetVotes.text = floor(doubtData.netVotes).toInt().toString()
-
         CoroutineScope(Dispatchers.Main).launch {
-            val currentState = votingUseCase.getUserCurrentState()
-
-            if (currentState == VotingUseCase.UPVOTED) {
-                ivUpvotes.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_baseline_thumb_up_filled))
-        //      ivUpvotes.setColorFilter(ContextCompat.getColor(itemView.context, R.color.purple), android.graphics.PorterDuff.Mode.SRC_IN);
+            when (votingUseCase.getUserCurrentState()) {
+                VotingUseCase.UPVOTED -> ivDownvotes.isClickable = false
+                VotingUseCase.DOWNVOTED -> ivUpvotes.isClickable = false
+                else -> {
+                    ivDownvotes.isClickable = true
+                    ivUpvotes.isClickable = true
+                }
             }
-
-            if (currentState == VotingUseCase.DOWNVOTED)
-                ivDownvotes.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_baseline_thumb_up_filled))
         }
     }
 }
