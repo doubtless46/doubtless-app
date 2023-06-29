a2402cfb631788def6098c2bc1b8557ebc4ad8c9
Aditya <adityasharma131003@gmail.com>
2023-06-29 13:24:06 +0530

    Added Analytics in Filter Tags

--- app/src/main/java/com/doubtless/doubtless/screens/doubt/HomeMainScreenFragment.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/doubt/HomeMainScreenFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.doubtless.doubtless.DoubtlessApp
+import com.doubtless.doubtless.analytics.AnalyticsTracker
 import com.doubtless.doubtless.databinding.FragmentHomeMainScreenBinding
 import com.doubtless.doubtless.navigation.FragNavigator
+import com.doubtless.doubtless.screens.auth.usecases.UserManager
 import com.doubtless.doubtless.screens.doubt.view.HomeMainScreenViewModel
 import com.doubtless.doubtless.screens.main.MainActivity
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import java.util.Locale
 
@@ -20,7 +23,8 @@ class HomeMainScreenFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var navigator: FragNavigator
     private lateinit var viewModel: HomeMainScreenViewModel
-    private lateinit var user: com.doubtless.doubtless.screens.auth.User
+    private lateinit var userManager: UserManager
+    private lateinit var analyticsTracker: AnalyticsTracker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -32,7 +36,8 @@ class HomeMainScreenFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        user = DoubtlessApp.getInstance().getAppCompRoot().getUserManager().getCachedUserData()!!
+        userManager = DoubtlessApp.getInstance().getAppCompRoot().getUserManager()
+        analyticsTracker = DoubtlessApp.getInstance().getAppCompRoot().getAnalyticsTracker()
 
         val _navigator = DoubtlessApp.getInstance().getAppCompRoot()
             .getHomeFragNavigator(requireActivity() as MainActivity)
@@ -70,10 +75,23 @@ class HomeMainScreenFragment : Fragment() {
                 ) else it.toString()
             }
         }.toMutableList()
-        capitalizedTagList[0] = "${user.local_user_attr!!.college!!} only"
+
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            if (position == 0) {
+                tab.text = "${userManager.getCachedUserData()?.local_user_attr!!.college!!} only"
+                return@TabLayoutMediator
+            }
             tab.text = capitalizedTagList[position]
         }.attach()
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                analyticsTracker.trackTagsFragment(capitalizedTagList[tab!!.position])
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+        })
 
     }
 
