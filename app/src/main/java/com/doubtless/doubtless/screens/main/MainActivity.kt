ccda296097850adc7a8b28ee979a645c9d04ae02
PratthamArora <arora.prattham@gmail.com>
2023-06-29 13:53:43 +0530

    added backpress confirmation on app exit

--- app/src/main/java/com/doubtless/doubtless/screens/main/MainActivity.kt
+++ app/src/main/java/com/doubtless/doubtless/screens/main/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.doubtless.doubtless.screens.main
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.doubtless.doubtless.R
 import com.doubtless.doubtless.databinding.ActivityMainBinding
 import com.doubtless.doubtless.navigation.BackPressDispatcher
 import com.doubtless.doubtless.navigation.OnBackPressListener
+import com.doubtless.doubtless.utils.shortToast
+
 
 class MainActivity : AppCompatActivity(), BackPressDispatcher {
 
+    private var doubleBackToExitPressedOnce: Boolean = false
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +25,7 @@ class MainActivity : AppCompatActivity(), BackPressDispatcher {
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.main_container, MainFragment(), "MainFragment")
+                .add(com.doubtless.doubtless.R.id.main_container, MainFragment(), "MainFragment")
                 .commit()
         }
     }
@@ -52,8 +57,22 @@ class MainActivity : AppCompatActivity(), BackPressDispatcher {
             }
         }
 
-        if (!backPressConsumed)
-            super.onBackPressed()
+        if (!backPressConsumed) {
+            when {
+                doubleBackToExitPressedOnce -> super.onBackPressed()
+                else -> {
+                    this.doubleBackToExitPressedOnce = true
+                    shortToast(R.string.press_again_to_exit)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        doubleBackToExitPressedOnce = false
+                    }, BACKPRESS_DELAY)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val BACKPRESS_DELAY = 2000L
     }
 
 }
